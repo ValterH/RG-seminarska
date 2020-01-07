2eb6e99128ee072a927c3dcfb639574057d0cb8f
unknown <valter.hudovernik@gmail.com>
2020-01-07 19:55:18 +0100

    rebuild

--- The Matrix/Node.js
+++ The Matrix/Node.js
@@ -1,28 +1,11 @@
-import Utils from './Utils.js';
-
-const vec3 = glMatrix.vec3;
 const mat4 = glMatrix.mat4;
-const quat = glMatrix.quat;
 
 export default class Node {
 
-    constructor(options) {
-        Utils.init(this, Node.defaults, options);
-
+    constructor() {
         this.transform = mat4.create();
-        this.updateTransform();
-
         this.children = [];
         this.parent = null;
-    }
-
-    updateTransform() {
-        const t = this.transform;
-        const degrees = this.rotation.map(x => x * 180 / Math.PI);
-        const q = quat.fromEuler(quat.create(), ...degrees);
-        const v = vec3.clone(this.translation);
-        const s = vec3.clone(this.scale);
-        mat4.fromRotationTranslationScale(t, q, v, s);
     }
 
     getGlobalTransform() {
@@ -48,21 +31,11 @@ export default class Node {
     }
 
     traverse(before, after) {
-        before && before(this);
+        before(this);
         for (let child of this.children) {
             child.traverse(before, after);
         }
-        after && after(this);
+        after(this);
     }
 
 }
-
-Node.defaults = {
-    translation: [0, 0, 0],
-    rotation: [0, 0, 0],
-    scale: [1, 1, 1],
-    aabb: {
-        min: [0, 0, 0],
-        max: [0, 0, 0],
-    },
-};
--- The Matrix/shaders.js
+++ The Matrix/shaders.js
@@ -2,14 +2,13 @@ const vertex = `#version 300 es
 layout (location = 0) in vec4 aPosition;
 layout (location = 1) in vec2 aTexCoord;
 
-uniform mat4 uViewModel;
-uniform mat4 uProjection;
+uniform mat4 uModelViewProjection;
 
 out vec2 vTexCoord;
 
 void main() {
     vTexCoord = aTexCoord;
-    gl_Position = uProjection * uViewModel * aPosition;
+    gl_Position = uModelViewProjection * aPosition;
 }
 `;
 
