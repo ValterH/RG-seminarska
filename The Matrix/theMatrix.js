92d4e1bd8b79fcb8bfb29dc40766ba0774b88baf
unknown <valter.hudovernik@gmail.com>
2020-01-08 13:04:34 +0100

    backup

--- The Matrix/theMatrix.js
+++ The Matrix/theMatrix.js
@@ -38,10 +38,9 @@ class App extends Application {
 
         this.initGL();
         this.initHandlers();
-
         const floorModel = this.createModel(FloorModel);
         const defaultTexture = WebGL.createTexture(gl, {
-            data   : new Uint8Array([255, 255, 255, 255]),
+            data   : new Uint8Array([117, 163, 186, 255]),
             width  : 1,
             height : 1,
         });
@@ -59,24 +58,113 @@ class App extends Application {
             translation      : vec3.set(vec3.create(), 0, 1, 0),
             velocity         : vec3.set(vec3.create(), 0, 0, 0),
             mouseSensitivity : 0.002,
-            maxSpeed         : 3,
+            maxSpeed         : 4,
             friction         : 0.2,
             acceleration     : 20
         });
+        //walls
+        this.wall1 = new Node();
+        this.wall1.model = floorModel;
+        this.wall1.texture = defaultTexture;
+        this.root.addChild(this.wall1);
+        mat4.fromRotationTranslationScale(this.wall1.transform, [0.7071,0,0,0.7071], [0,10,-10], [10, 1, 10]);
+        this.wall2 = new Node();
+        this.wall2.model = floorModel;
+        this.wall2.texture = defaultTexture;
+        this.root.addChild(this.wall2);
+        mat4.fromRotationTranslationScale(this.wall2.transform, [0,-0.7071,0.7071,0], [0,10,10], [10, 1, 10]);
+        this.wall3 = new Node();
+        this.wall3.model = floorModel;
+        this.wall3.texture = defaultTexture;
+        this.root.addChild(this.wall3);
+        mat4.fromRotationTranslationScale(this.wall3.transform, [0.5,-0.5,0.5,0.5], [10,10,0], [10, 1, 10]);
+        this.wall4 = new Node();
+        this.wall4.model = floorModel;
+        this.wall4.texture = defaultTexture;
+        this.root.addChild(this.wall4);
+        mat4.fromRotationTranslationScale(this.wall4.transform, [0.5,0.5,-0.5,0.5], [-10,10,0], [10, 1, 10]);
 
+        this.loadTexture('../../common/images/window.jpg', {
+            mip: true,
+            min: gl.NEAREST_MIPMAP_NEAREST,
+            mag: gl.NEAREST,
+        }, (texture) => {
+            this.wall1.texture = texture;
+            this.wall2.texture = texture;
+            this.wall3.texture = texture;
+            this.wall4.texture = texture;
+        });
+
+        //sky
+        this.sky = new Node();
+        this.sky.model = floorModel;
+        this.sky.texture = defaultTexture;
+        this.root.addChild(this.sky);
+        mat4.fromRotationTranslationScale(this.sky.transform, [1,0,0,0], [0,20,0], [10, 1, 10]);
+
+        //floor
         this.floor = new Node();
         this.floor.model = floorModel;
         this.floor.texture = defaultTexture;
         this.root.addChild(this.floor);
         mat4.fromScaling(this.floor.transform, [10, 1, 10]);
 
-        this.loadTexture('../../common/images/grass.png', {
+        this.t = 0;
+        this.loadTexture('../../common/images/tile.png', {
             mip: true,
             min: gl.NEAREST_MIPMAP_NEAREST,
             mag: gl.NEAREST,
         }, (texture) => {
             this.floor.texture = texture;
         });
+    }
+
+    toggleTexture() {
+        var text;
+        const gl = this.gl;
+        if(this.t == 1) {
+            text = '../../common/images/tile.png';
+            this.t = 0;
+            this.loadTexture('../../common/images/window.jpg', {
+                mip: true,
+                min: gl.NEAREST_MIPMAP_NEAREST,
+                mag: gl.NEAREST,
+            }, (texture) => {
+                this.wall1.texture = texture;
+                this.wall2.texture = texture;
+                this.wall3.texture = texture;
+                this.wall4.texture = texture;
+            });
+            this.loadTexture('../../common/images/tile.png', {
+                mip: true,
+                min: gl.NEAREST_MIPMAP_NEAREST,
+                mag: gl.NEAREST,
+            }, (texture) => {
+                this.floor.texture = texture;
+            });
+            const defaultTexture = WebGL.createTexture(gl, {
+                data   : new Uint8Array([117, 163, 186, 255]),
+                width  : 1,
+                height : 1,
+            });
+            this.sky.texture = defaultTexture;
+        } else {
+            text = '../../common/images/matrix.jpg';
+            this.t = 1;
+        
+            this.loadTexture(text, {
+                mip: true,
+                min: gl.NEAREST_MIPMAP_NEAREST,
+                mag: gl.NEAREST,
+            }, (texture) => {
+                this.floor.texture = texture;
+                this.wall1.texture = texture;
+                this.wall2.texture = texture;
+                this.wall3.texture = texture;
+                this.wall4.texture = texture;
+                this.sky.texture = texture;
+            });
+        }
     }
 
     update() {
@@ -94,15 +182,19 @@ class App extends Application {
         // 1: add movement acceleration
         let acc = vec3.create();
         if (this.keys['KeyW']) {
+            if(this.keys['KeyQ']) vec3.add(acc, acc, forward);
             vec3.add(acc, acc, forward);
         }
         if (this.keys['KeyS']) {
+            if(this.keys['KeyQ']) vec3.add(acc, acc, forward);
             vec3.sub(acc, acc, forward);
         }
         if (this.keys['KeyD']) {
+            if(this.keys['KeyQ']) vec3.sub(acc, acc, right);
             vec3.add(acc, acc, right);
         }
         if (this.keys['KeyA']) {
+            if(this.keys['KeyQ']) vec3.add(acc, acc, right);
             vec3.sub(acc, acc, right);
         }
 
@@ -120,7 +212,7 @@ class App extends Application {
 
         // 4: limit speed
         const len = vec3.len(c.velocity);
-        if (len > c.maxSpeed) {
+        if ((len > c.maxSpeed && !this.keys['KeyQ']) || (this.keys['KeyQ'] && len * 2 > c.maxSpeed)) {
             vec3.scale(c.velocity, c.velocity, c.maxSpeed / len);
         }
 
@@ -172,6 +264,7 @@ class App extends Application {
 
     keydownHandler(e) {
         this.keys[e.code] = true;
+        if(e.code == "KeyM") this.toggleTexture();
     }
 
     keyupHandler(e) {
@@ -261,42 +354,37 @@ class App extends Application {
 
 }
 
+
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.querySelector('canvas');
     const app = new App(canvas);
-    const gui = new dat.GUI();
-    gui.add(app.camera, 'mouseSensitivity', 0.0001, 0.01);
-    gui.add(app.camera, 'maxSpeed', 0, 10);
-    gui.add(app.camera, 'friction', 0.05, 0.75);
-    gui.add(app.camera, 'acceleration', 1, 100);
-    gui.add(app, 'enableMouseLook');
+    app.enableMouseLook();
 });
-
-
 
 
 /*
         PAUSE UNPAUSE
 */
+
+document.addEventListener("keydown", pause);
+
 function pause(e) {
     if(e.key === "p"){
-        var item= document.getElementById("pause");
+        var item = document.getElementById("pause");
         if(item.style.display === "block") {
+            var canvas = document.getElementById("canvas");
+            canvas.requestPointerLock()
             item.style.display = "none";
-            app.enableCamera();
+            document.requestPointerLock;
         } else {
             item.style.display = "block";
-            app.pointerlockchangeHandler();
+            document.exitPointerLock();
         }
     }
 }
 
-function unpause() {
-item = document.getElementById("pause").style.display = "block";
-app.enableCamera();
-}
 
-document.addEventListener("keydown", pause);
+
 
 
 
